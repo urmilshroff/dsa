138a781890399955d03e44a5e03c1d117c19bbe7
urmilshroff <urmilshroff@gmail.com>
2019-09-05 03:10:47 +0530

    Update doubly_ll.py

--- linked_lists/doubly_ll.py
+++ linked_lists/doubly_ll.py
@@ -8,6 +8,7 @@ class Node:
 class DoublyLL:
     def __init__(self):
         self.head = None
+        self.tail = None
 
     def display_list(self):
         if self.head is None:
@@ -22,21 +23,43 @@ class DoublyLL:
     def insert_head(self, val):
         if self.head is None:
             self.head = Node(val)
+            self.head.next = self.tail
 
         else:
-            self.head.previous = Node(val)
+            current = Node(val)
+            self.head.previous = current
             current.next = self.head
-            self.head = Node(val)
+            self.head = current
 
     def insert_tail(self, val):
         if self.head is None:
-            self.head = Node(val)
+            self.insert_head(val)
+
+        elif self.tail is None:
+            self.tail = Node(val)
+
+        else:
+            current = Node(val)
+            self.tail.next = current
+            current.previous = self.tail
+
+    def delete_head(self):
+        if self.head is None:
+            print('No head to delete')
 
         else:
             current = self.head
-            while current.next is not None:
-                current = current.next
-            current.next = Node(val)
+            self.head = self.head.next
+            current = None
+
+    def delete_tail(self):
+        if self.tail is None:
+            print('No tail to delete')
+
+        else:
+            current = self.tail
+            self.tail = self.tail.previous
+            current = None
 
 
 list = DoublyLL()
@@ -44,15 +67,15 @@ list = DoublyLL()
 list.display_list()  # empty
 list.insert_head(1)
 list.insert_tail(2)
-list.display_list()  # 1 2
+# list.display_list()  # 1 2
 list.insert_tail(3)
 list.delete_head()
-list.display_list()  # 2 3
+# list.display_list()  # 2 3
 list.insert_tail(4)
-list.search(4)  # 4 found in position 4
+# list.search(4)  # 4 found in position 4
 list.display_list()  # 4 2 3
 list.delete_tail()
-list.search(4)  # not found
+# list.search(4)  # not found
 list.delete_tail()
 list.delete_tail()
 list.display_list()  # empty
