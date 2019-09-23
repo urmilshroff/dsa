470e8eb7b0eb276626f7b774b615d77eca8cb94c
urmilshroff <urmilshroff@gmail.com>
2019-09-24 00:56:02 +0530

    Create stack-ll.py

---
+++ stacks/stack-ll.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class Stack:
+    def __init__(self, size):
+        self.size = size
+        self.top = None
+
+    def push(item):
+        pass
+
+    def pop():
+        pass
+
+    def peek():
+        pass
+
+    def is_empty():
+        if self.top is None:
+            return True
+        else:
+            return False
+
+
+stack = Stack(size=5)
