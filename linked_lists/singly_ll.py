c787e1e5a432481e308f466682383f4fd90079e5
urmilshroff <urmilshroff@gmail.com>
2020-01-19 00:50:53 +0530

    Update singly_ll.py

--- linked_lists/singly_ll.py
+++ linked_lists/singly_ll.py
@@ -8,85 +8,97 @@ class SinglyLL:
     def __init__(self):
         self.head = None
 
-    def display_list(self):
+    def insert_beg(self, val):
+        new_node = Node(val)
+
         if self.head is None:
-            print('Linked list is empty')
+            self.head = new_node
+
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    def insert_end(self, val):
+        new_node = Node(val)
+
+        if self.head is None:
+            self.head = new_node
 
         else:
             current = self.head
-            while current is not None:
-                print(current.val)
+            while current.next is not None:
                 current = current.next
+            current.next = new_node
 
-    def search(self, find):
+    def insert_at(self):
+        pass
+
+    def delete_beg(self):
+        if self.head is None:
+            print('Linked list is empty')
+
+        else:
+            new_head = self.head.next
+            self.head = None
+            self.head = new_head
+
+    def delete_end(self):
         if self.head is None:
             print('Linked list is empty')
 
         else:
             current = self.head
-            pos = 0
-
-            while current is not None:
-                if current.val == find:
-                    print(f'{find} found in position {pos}')
+            while current.next is not None:
+                current = current.next
+                if current.next is None:
+                    current.val = None  # fix
                     break
 
-                pos += 1
+    def delete_at(self):
+        pass
+
+    def search(self, elt):
+        if self.head is None:
+            print('Linked list is empty')
+
+        else:
+            current = self.head
+            while current is not None:
+                if current.val == elt:
+                    print(f'{elt} found!')
+                    break
+                else:
+                    current = current.next
+
+    def reverse(self):
+        if self.head is None:
+            print('Linked list is empty')
+
+        else:
+            pass
+
+    def display(self):
+        if self.head is None:
+            print('Linked list is empty')
+
+        else:
+            print('Linked list is', end=' ')
+            current = self.head
+            while current is not None:
+                print(current.val, end=' ')
                 current = current.next
-
-    def insert_head(self, val):
-        if self.head is None:
-            self.head = Node(val)
-
-        else:
-            current = Node(val)
-            current.next = self.head
-            self.head = current
-
-    def insert_tail(self, val):
-        if self.head is None:
-            self.head = Node(val)
-
-        else:
-            current = self.head
-            while current.next is not None:
-                current = current.next
-            current.next = Node(val)
-
-    def delete_head(self):
-        if self.head is None:
-            print('No head to delete')
-
-        else:
-            current = self.head
-            self.head = self.head.next
-            current = None
-
-    def delete_tail(self):
-        if self.head is None:
-            print('No tail to delete')
-
-        else:
-            current = self.head
-            while current.next is not None:
-                current = current.next  # reach the end of the list
-            current = None
+            print()
 
 
 list = SinglyLL()
 
-list.display_list()  # empty
-list.insert_head(1)
-list.insert_tail(2)
-list.display_list()  # 1 2
-list.insert_tail(3)
-list.delete_head()
-list.display_list()  # 2 3
-list.insert_tail(4)
-list.search(4) # 4 found in position 4
-list.display_list()  # 4 2 3
-list.delete_tail()
-list.search(4)  # not found
-list.delete_tail()
-list.delete_tail()
-list.display_list()  # empty
+list.display()
+list.insert_beg(1)
+list.insert_end(2)
+list.search(2)
+list.insert_end(3)
+list.insert_end(4)
+list.delete_end()
+list.search(4)
+
+list.display()
