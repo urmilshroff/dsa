7f2e3d7b075da08a6b1375ca55e8ce71a5bd253d
urmilshroff <urmilshroff@gmail.com>
2020-01-21 01:33:33 +0530

    Update doubly_ll.py

--- linked_lists/doubly_ll.py
+++ linked_lists/doubly_ll.py
@@ -8,12 +8,31 @@ class Node:
 class DoublyLL:
     def __init__(self):
         self.head = None
+        self.tail = None
 
-    def insert_beg(self):
-        pass
+    def insert_beg(self, val):
+        new_node = Node(val)
 
-    def insert_end(self):
-        pass
+        if self.head is None or self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
+
+    def insert_end(self, val):
+        new_node = Node(val)
+
+        if self.head is None or self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            self.tail.next = new_node
+            new_node.prev = new_node.prev
+            self.tail = new_node
 
     def insert_at(self):
         pass
@@ -31,7 +50,14 @@ class DoublyLL:
         pass
 
     def reverse(self):
-        pass
+        if self.head is None or self.tail is None:
+            print('Linked list is empty')
+
+        else:
+            current = self.tail
+            while current is not None:
+                print(current.val, end='')
+                current = current.prev
 
     def display(self):
         pass
